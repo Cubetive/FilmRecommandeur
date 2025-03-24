56d7dd9f848a746d6752ad253eac1b2b5b1a8a2a
Cubetive <peternguyenstudent123@gmail.com>
2025-03-24 11:34:08 -0400

    stuff

---
+++ graph.py
@@ -0,0 +1,4 @@
+"""CSC111 Project 2: OptiTransport - Graph
+
+This Python module contains the Graph strucutre code for Project 2: OptiTransport.
+"""
--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-"""Main file
+"""CSC111 Project 2: OptiTransport - Main
+
+This Python module contains the main code for Project 2: OptiTransport.
+Please consult Minh Nguyen at huynhtuanminh.nguyen@mail.utoronto.ca
+for more information.
+
+Copyright and Usage Information
+===============================
+
+
 """
 
 if __name__ == "__main__":
